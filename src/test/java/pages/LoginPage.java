f0f743c38963af28e636166a8a3c7e342401a236
Himanshu Maurya <himanshu.maurya@3ds.com>
2022-09-11 11:02:27 +0530

    First Commit Selenium_Maven_TestNG

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,54 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+	WebDriver driver;
+	
+//	*************************Objects*******************************
+	@FindBy(linkText = "Log in")
+	WebElement LoginLink;
+	
+	@FindBy(name = "user_login")
+	WebElement UserName;
+	
+	@FindBy(id = "password")
+	WebElement Password;
+	
+	@FindBy(className = "rememberMe")
+	WebElement Checkbox;
+	
+	@FindBy(name = "btn_login")
+	WebElement LoginBtn;
+	
+	
+	public LoginPage(WebDriver basedriver) {
+		this.driver=basedriver;
+		PageFactory.initElements(basedriver, this);
+	}
+	
+//	*************************Methods*******************************
+	public void Login(String UserNameVal, String PasswordVal) {
+		// Step1. Click  on the Login Link
+		LoginLink.click();
+		
+		//Step2: Enter Username
+		UserName.sendKeys(UserNameVal);
+		
+		
+		//Step3: Enter Password
+		Password.sendKeys(PasswordVal);
+		
+		//Step4 Click Remember Me checkbox
+        Checkbox.click();
+		
+		//Step5: Click pn Login Button
+		LoginBtn.click();
+	}
+
+	
+}
